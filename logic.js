a752e7deaba3996221ff4cb233521ad49ed07bec
prakhar-161 <pdhama161@gmail.com>
2021-07-16 11:38:33 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,9 @@
+function Calculate()
+{
+    let p = document.getElementById('amount').value;
+    let t = document.getElementById('time').value;
+    let r = document.getElementById('rate').value;
+    let si = (p*t*r)/100;
+
+    document.getElementById('result').innerHTML="Your Simple Interest is: " + si; 
+}
